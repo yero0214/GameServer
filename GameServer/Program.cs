9f9c9e6064671b6a09d9c18a85341587419dae08
yero0214 <yero0214@gmail.com>
2021-05-04 18:42:34 +0900

    Add project files.

---
+++ GameServer/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace GameServer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Game SErver";
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ GameServer/Server.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Net;
+using System.Net.Sockets;
+
+namespace GameServer
+{
+    class Server
+    {
+        public static int MaxPlayers { get; private set; } 
+        public static int Port { get; private set; }
+
+        private static TcpListener tcpListener;
+        public static void Start(int _maxPlayers, int _port)
+        {
+            MaxPlayers = _maxPlayers;
+            Port = _port;
+
+            Console.WriteLine("Starting server...");
+
+            tcpListener = new TcpListener(IPAddress.Any, Port);
+            tcpListener.Start();
+            tcpListener.BeginAcceptTcpClient(new AsyncCallback(TCPConnectCallback), null);
+
+            Console.WriteLine($"Server started on {Port}.");
+        }
+
+        private static void TCPConnectCallback(IAsyncResult _result)
+        {
+            TcpClient _client = tcpListener.EndAcceptTcpClient(_result);
+            tcpListener.BeginAcceptTcpClient(new AsyncCallback(TCPConnectCallback), null);
+        }
+    }
+}
